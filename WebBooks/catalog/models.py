a125933fb1f27af137a0dd682b5bbb21059cefb3
Your Name <you@example.com>
2024-11-12 09:07:25 +0300

    PZ8_1

--- WebBooks/catalog/models.py
+++ WebBooks/catalog/models.py
@@ -27,7 +27,7 @@ class Book(models.Model):
     help_text="Введите название книги", verbose_name="Название книги")
     genre = models.ForeignKey('Genre', on_delete=models.CASCADE, help_text=" Выберите жанр для книги", verbose_name="Жанр книги", null=True)
     language = models.ForeignKey('Language', on_delete=models.CASCADE, help_text="Выберите язык книги", verbose_name="Язык книги", null=True)
-    author = models.ManyToManyField('Author', help_text="Выберите автора книги", verbose_name="Автор книги", null=True)
+    author = models.ManyToManyField('Author', help_text="Выберите автора книги", verbose_name="Автор книги")
     summary = models.TextField(max_length=1000, help_text="Введите краткое описание книги", verbose_name="Аннотация книги")
     isbn = models.CharField(max_length=13, help_text="Должно содержать 13 символов", verbose_name="ISBN книги")
 def __str__(self):
@@ -43,7 +43,7 @@ class Status(models.Model):
 def __str__(self):
     return self.name
 
-class BookInstance(models.Model):
+class Bookinstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.CASCADE, null=True)
     inv_nom = models.CharField(max_length=20, null=True, help_text="Введите инвентарный номер экземпляра", verbose_name="Инвентарный номер")
     imprint = models.CharField(max_length=200, help_text="Введите издательство и год выпуска", verbose_name="Издательство")
