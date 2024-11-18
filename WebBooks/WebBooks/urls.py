40c576b837a6ffe65c14444c442ddbd62d233c6c
Your Name <you@example.com>
2024-11-18 12:52:10 +0300

    LZ13_2

--- WebBooks/WebBooks/urls.py
+++ WebBooks/WebBooks/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from catalog import views
 urlpatterns = [
+     path('', views.index),
  path('', views.index, name='home'),
  path('admin/', admin.site.urls),
 ]
--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from catalog.models import Book, Bookinstance, Author
+
 # Create your views here.
 def index(request):
- return HttpResponse("Главная страница сайта Мир книг!")
+ # Генерация "количеств" некоторых главных объектов
+ num_books = Book.objects.all().count()
+ num_instances = Bookinstance.objects.all().count()
+ # Доступные книги (статус = 'На складе')
+ num_instances_available = Bookinstance.objects.filter(status__exact=2).count()
+ num_authors = Author.objects.count() # Метод 'all()' применен по умолчанию.
+ # Количество посещений этого view, подсчитанное
+ # в переменной session
+ num_visits = request.session.get('num_visits', 0)
+ request.session['num_visits'] = num_visits + 1
+ # Отрисовка HTML-шаблона index.html с данными внутри переменной context
+ return render(request, 'index.html',
+ context={'num_books': num_books,
+ 'num_instances': num_instances,
+ 'num_instances_available':
+ num_instances_available,
+ 'num_authors': num_authors,
+ 'num_visits': num_visits},
+ )
+
+
