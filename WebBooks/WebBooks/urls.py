3a2bbf50885194145d8f97603ee894bac6dd68e5
Your Name <you@example.com>
2024-11-19 12:15:55 +0300

    LZ13_3

--- WebBooks/WebBooks/urls.py
+++ WebBooks/WebBooks/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path, include
 from django.contrib import admin
 from django.urls import path
 from catalog import views
@@ -21,4 +22,7 @@ urlpatterns = [
      path('', views.index),
  path('', views.index, name='home'),
  path('admin/', admin.site.urls),
+]
+urlpatterns += [
+ path('accounts/', include('django.contrib.auth.urls')),
 ]
